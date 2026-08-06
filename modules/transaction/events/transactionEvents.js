3cc2d62f79f9971215195c6635727158dd8d888a
try102 <jiaxd82@gmail.com>
2026-08-06 13:13:21 +0800

    Create transactionEvents.js

---
+++ modules/transaction/events/transactionEvents.js
@@ -0,0 +1,25 @@
+/*
+
+Family Wealth AI OS
+
+Transaction Events
+
+*/
+
+const TransactionEvents = {
+
+    CREATED:
+
+    "TRANSACTION_CREATED",
+
+    UPDATED:
+
+    "TRANSACTION_UPDATED",
+
+    DELETED:
+
+    "TRANSACTION_DELETED"
+
+};
+
+export default TransactionEvents;
