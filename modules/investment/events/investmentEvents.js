1721b028d72b18ff3a68011105b58109edb4296f
try102 <jiaxd82@gmail.com>
2026-08-06 14:15:09 +0800

    Create investmentEvents.js

---
+++ modules/investment/events/investmentEvents.js
@@ -0,0 +1,41 @@
+/*
+
+Family Wealth AI OS
+
+Investment Events
+
+*/
+
+const InvestmentEvents = {
+
+    INVESTMENT_CREATED:
+
+    "investment.created",
+
+    POSITION_UPDATED:
+
+    "investment.position.updated",
+
+    TRADE_CREATED:
+
+    "investment.trade.created",
+
+    BUY_EXECUTED:
+
+    "investment.buy.executed",
+
+    SELL_EXECUTED:
+
+    "investment.sell.executed",
+
+    DIVIDEND_RECEIVED:
+
+    "investment.dividend.received",
+
+    PORTFOLIO_CHANGED:
+
+    "investment.portfolio.changed"
+
+};
+
+export default InvestmentEvents;
