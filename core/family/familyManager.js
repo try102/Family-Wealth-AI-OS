f8f2a975501dc418f261ff5c8d5bb1042fb7b368
try102 <jiaxd82@gmail.com>
2026-08-06 11:10:16 +0800

    Update familyManager.js

--- core/family/familyManager.js
+++ core/family/familyManager.js
@@ -1,1 +1,67 @@
+/*
 
+Family Wealth AI OS
+
+Family Manager
+
+*/
+
+import DataService from "../database/dataService.js";
+
+const FAMILY_KEY =
+
+"family_profile";
+
+const FamilyManager = {
+
+    create(family){
+
+        DataService.save(
+
+            FAMILY_KEY,
+
+            family
+
+        );
+
+        return family;
+
+    },
+
+    get(){
+
+        return DataService.load(
+
+            FAMILY_KEY
+
+        );
+
+    },
+
+    update(family){
+
+        DataService.save(
+
+            FAMILY_KEY,
+
+            family
+
+        );
+
+        return family;
+
+    },
+
+    remove(){
+
+        DataService.remove(
+
+            FAMILY_KEY
+
+        );
+
+    }
+
+};
+
+export default FamilyManager;
