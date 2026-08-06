c7c17e060dfb1b300dd167ba0ce78a46d271b3a7
try102 <jiaxd82@gmail.com>
2026-08-06 11:02:30 +0800

    Update eventTypes.js

--- core/events/eventTypes.js
+++ core/events/eventTypes.js
@@ -1,1 +1,61 @@
+/*
 
+Family Wealth AI OS
+
+System Event Definitions
+
+*/
+
+const EventTypes = {
+
+    // Asset
+
+    ASSET_CREATED:
+
+    "ASSET_CREATED",
+
+    ASSET_UPDATED:
+
+    "ASSET_UPDATED",
+
+    ASSET_DELETED:
+
+    "ASSET_DELETED",
+
+    // Account
+
+    ACCOUNT_CREATED:
+
+    "ACCOUNT_CREATED",
+
+    // Transaction
+
+    TRANSACTION_CREATED:
+
+    "TRANSACTION_CREATED",
+
+    // Investment
+
+    TRADE_EXECUTED:
+
+    "TRADE_EXECUTED",
+
+    // Cash Flow
+
+    INCOME_RECEIVED:
+
+    "INCOME_RECEIVED",
+
+    EXPENSE_RECORDED:
+
+    "EXPENSE_RECORDED",
+
+    // Tax
+
+    TAX_PROFILE_UPDATED:
+
+    "TAX_PROFILE_UPDATED"
+
+};
+
+export default EventTypes;
