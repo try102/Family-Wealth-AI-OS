852b84dc5ff683de609fdc7d523c7d1de3bbab93
try102 <jiaxd82@gmail.com>
2026-08-06 12:46:16 +0800

    Create accountEvents.js

---
+++ modules/account/events/accountEvents.js
@@ -0,0 +1,25 @@
+/*
+
+Family Wealth AI OS
+
+Account Events
+
+*/
+
+const AccountEvents = {
+
+    CREATED:
+
+    "ACCOUNT_CREATED",
+
+    UPDATED:
+
+    "ACCOUNT_UPDATED",
+
+    DELETED:
+
+    "ACCOUNT_DELETED"
+
+};
+
+export default AccountEvents;
