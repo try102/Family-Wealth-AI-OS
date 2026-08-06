fa8cab1c6a03334dc8acf50cab91b618787ca3b5
try102 <jiaxd82@gmail.com>
2026-08-06 15:11:22 +0800

    Create logger.test.js

---
+++ core/utils/tests/logger.test.js
@@ -0,0 +1,69 @@
+/*
+
+Family Wealth AI OS
+
+Logger Test
+
+*/
+
+import Logger from "../logger.js";
+
+// =====================
+
+// Info
+
+// =====================
+
+Logger.info(
+
+    "System started"
+
+);
+
+// =====================
+
+// Warn
+
+// =====================
+
+Logger.warn(
+
+    "Memory usage is high"
+
+);
+
+// =====================
+
+// Error
+
+// =====================
+
+Logger.error(
+
+    "Test error message"
+
+);
+
+// =====================
+
+// Debug
+
+// =====================
+
+Logger.debug(
+
+    "Debug message"
+
+);
+
+// =====================
+
+// Complete
+
+// =====================
+
+console.log(
+
+    "Logger Test Passed"
+
+);
