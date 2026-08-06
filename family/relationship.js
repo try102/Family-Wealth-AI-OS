0fc3b9a8e0affb200beab9ee156ad9e4a748ef24
try102 <jiaxd82@gmail.com>
2026-08-06 20:22:41 +0800

    Create relationship.js

---
+++ family/relationship.js
@@ -0,0 +1,57 @@
+/*
+
+Family Wealth AI OS V7.1
+
+Family Relationship
+
+*/
+
+const Relationship = {
+
+    SELF:
+
+    "SELF",
+
+    SPOUSE:
+
+    "SPOUSE",
+
+    SON:
+
+    "SON",
+
+    DAUGHTER:
+
+    "DAUGHTER",
+
+    FATHER:
+
+    "FATHER",
+
+    MOTHER:
+
+    "MOTHER",
+
+    BROTHER:
+
+    "BROTHER",
+
+    SISTER:
+
+    "SISTER",
+
+    GRANDPARENT:
+
+    "GRANDPARENT",
+
+    GRANDCHILD:
+
+    "GRANDCHILD",
+
+    OTHER:
+
+    "OTHER"
+
+};
+
+export default Relationship;
