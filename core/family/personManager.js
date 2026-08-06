7b953ebde4dfcc10245fcee04af5399082ad6c20
try102 <jiaxd82@gmail.com>
2026-08-06 11:11:40 +0800

    Update personManager.js

--- core/family/personManager.js
+++ core/family/personManager.js
@@ -1,1 +1,87 @@
+/*
 
+Family Wealth AI OS
+
+Person Manager
+
+*/
+
+import DataService from "../database/dataService.js";
+
+const PERSON_KEY =
+
+"family_persons";
+
+const PersonManager = {
+
+    add(person){
+
+        let persons =
+
+        DataService.load(
+
+            PERSON_KEY
+
+        ) || [];
+
+        persons.push(
+
+            person
+
+        );
+
+        DataService.save(
+
+            PERSON_KEY,
+
+            persons
+
+        );
+
+        return person;
+
+    },
+
+    list(){
+
+        return (
+
+            DataService.load(
+
+                PERSON_KEY
+
+            )
+
+            || []
+
+        );
+
+    },
+
+    remove(id){
+
+        let persons =
+
+        this.list();
+
+        persons =
+
+        persons.filter(
+
+            p=>p.id!==id
+
+        );
+
+        DataService.save(
+
+            PERSON_KEY,
+
+            persons
+
+        );
+
+    }
+
+};
+
+export default PersonManager;
