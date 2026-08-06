0acda0a902b66614e6356a5886d48c1a0af5611e
try102 <jiaxd82@gmail.com>
2026-08-06 20:10:02 +0800

    Create member.js

---
+++ family/member.js
@@ -0,0 +1,63 @@
+/*
+
+Family Wealth AI OS V7.1
+
+Family Manager
+
+*/
+
+import Member from "./member.js";
+
+const FamilyManager = {
+
+    members: [],
+
+    add(member){
+
+        if(!(member instanceof Member)){
+
+            throw new Error("Invalid member");
+
+        }
+
+        this.members.push(member);
+
+        return member;
+
+    },
+
+    list(){
+
+        return this.members;
+
+    },
+
+    get(id){
+
+        return this.members.find(
+
+            member => member.id === id
+
+        );
+
+    },
+
+    remove(id){
+
+        this.members = this.members.filter(
+
+            member => member.id !== id
+
+        );
+
+    },
+
+    clear(){
+
+        this.members = [];
+
+    }
+
+};
+
+export default FamilyManager;
