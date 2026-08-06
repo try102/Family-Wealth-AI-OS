72142e708300e13b481b48deb6eabb47fc053904
try102 <jiaxd82@gmail.com>
2026-08-06 11:44:28 +0800

    Create assetEvents.js

---
+++ modules/asset/events/assetEvents.js
@@ -0,0 +1,25 @@
+/*
+
+Family Wealth AI OS
+
+Asset Events
+
+*/
+
+const AssetEvents = {
+
+    CREATED:
+
+    "ASSET_CREATED",
+
+    UPDATED:
+
+    "ASSET_UPDATED",
+
+    DELETED:
+
+    "ASSET_DELETED"
+
+};
+
+export default AssetEvents;
