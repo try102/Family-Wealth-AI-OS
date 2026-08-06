8b07ce9aa73f92938c096fcdaf1600349bec8c01
try102 <jiaxd82@gmail.com>
2026-08-06 10:56:22 +0800

    Update storageAdapter.js

--- core/database/storageAdapter.js
+++ core/database/storageAdapter.js
@@ -1,1 +1,45 @@
+/*
 
+Family Wealth AI OS
+
+Storage Adapter
+
+*/
+
+const StorageAdapter = {
+
+    get(key){
+
+        return localStorage.getItem(
+
+            key
+
+        );
+
+    },
+
+    set(key,value){
+
+        localStorage.setItem(
+
+            key,
+
+            value
+
+        );
+
+    },
+
+    delete(key){
+
+        localStorage.removeItem(
+
+            key
+
+        );
+
+    }
+
+};
+
+export default StorageAdapter;
