eeeeb4ecebfb20a8765905d201dc0b8b24ca5152
try102 <jiaxd82@gmail.com>
2026-08-06 10:55:07 +0800

    Update dataService.js

--- core/database/dataService.js
+++ core/database/dataService.js
@@ -1,1 +1,51 @@
+/*
 
+Family Wealth AI OS
+
+Core Database Service
+
+*/
+
+const DataService = {
+
+    save(key, data){
+
+        localStorage.setItem(
+
+            key,
+
+            JSON.stringify(data)
+
+        );
+
+    },
+
+    load(key){
+
+        const data =
+
+        localStorage.getItem(key);
+
+        if(!data){
+
+            return null;
+
+        }
+
+        return JSON.parse(data);
+
+    },
+
+    remove(key){
+
+        localStorage.removeItem(
+
+            key
+
+        );
+
+    }
+
+};
+
+export default DataService;
