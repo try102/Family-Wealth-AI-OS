9fddfe278b41be45183db9b7923a0ad99b842528
try102 <jiaxd82@gmail.com>
2026-08-06 11:07:54 +0800

    Update moduleRegistry.js

--- core/modules/moduleRegistry.js
+++ core/modules/moduleRegistry.js
@@ -1,1 +1,57 @@
+/*
 
+Family Wealth AI OS
+
+Module Registry
+
+*/
+
+const modules = {};
+
+const ModuleRegistry = {
+
+    register(
+
+        name,
+
+        module
+
+    ){
+
+        modules[name] = module;
+
+    },
+
+    get(
+
+        name
+
+    ){
+
+        return modules[name];
+
+    },
+
+    list(){
+
+        return Object.keys(
+
+            modules
+
+        );
+
+    },
+
+    remove(
+
+        name
+
+    ){
+
+        delete modules[name];
+
+    }
+
+};
+
+export default ModuleRegistry;
